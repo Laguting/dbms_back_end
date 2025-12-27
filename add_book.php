76c3854f814730f5b91d142a76a51f770c740573
Laguting <lagutingmaricon@gmail.com>
2025-12-28 02:50:50 +0800

    Form wrapping

--- add_book.php
+++ add_book.php
@@ -1,17 +1,19 @@
 <?php
 include "db_connect.php";
 
-$isbn = $_POST['isbn'];
-$title = $_POST['title'];
-$author = $_POST['author'];
-$price = $_POST['price'];
+if (isset($_POST['submit'])) {  // only run when form is submitted
+    $isbn = $_POST['isbn'];
+    $title = $_POST['title'];
+    $author = $_POST['author'];
+    $price = $_POST['price'];
 
-$sql = "INSERT INTO book (isbn, title, author, selling_price)
-        VALUES ('$isbn', '$title', '$author', '$price')";
+    $sql = "INSERT INTO book (isbn, title, author, selling_price)
+            VALUES ('$isbn', '$title', '$author', '$price')";
 
-if (mysqli_query($conn, $sql)) {
-    echo "Book added successfully!";
-} else {
-    echo "Error: " . mysqli_error($conn);
+    if (mysqli_query($conn, $sql)) {
+        echo "Book added successfully!";
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
 }
 ?>
