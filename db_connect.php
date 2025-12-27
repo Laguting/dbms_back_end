bd1c6877bce6c1bf57d8d1a57f7c95d093f0f54d
Laguting <lagutingmaricon@gmail.com>
2025-12-28 02:41:56 +0800

    added database succel connection notification

--- db_connect.php
+++ db_connect.php
@@ -1,13 +1,17 @@
 <?php
 $host = "localhost";
 $user = "root";
-$password = "";   // NOT empty
+$password = ""; // default XAMPP
 $database = "ink_and_solace";
 $port = 3307;
 
+// Connect to database
 $conn = mysqli_connect($host, $user, $password, $database, $port);
 
+// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Database connected successfully!";
 }
 ?>
